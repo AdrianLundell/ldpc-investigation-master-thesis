87e90ebd930c5ffd95871ad6e5b9400f3f7a639e
Adrian Lundell <adrian.lundell@gmail.com>
2022-03-23 14:40:49 +0100

    Add demoduation with static thresholds

--- aff3ct_alpha/src/main.py
+++ aff3ct_alpha/src/main.py
@@ -1,6 +1,7 @@
 #include <aff3ct.hpp>
 #using namespace aff3ct;
 #%%i
+from cmath import inf
 import numpy as np
 import matplotlib.pyplot as plt 
 
@@ -36,18 +37,38 @@ class Modem(Module):
                 symbols[:] = np.vectorize(mapping.get)(binary_symbols)
                 
 
-        def demodulate(self):
+        def demodulate(self, noisy_symbols, LLRs):
                 """Demodulates a sequence of N/M symbols into a sequence of N*S bits, S being the number of soft bits per read"""
-                pass 
+                m = int(np.log2(len(self.p["voltage_levels"])))
+                q = int(self.p["block_length"]//m) 
+                binary_symbols = np.zeros(q, np.uint8)
+
+                if self.p["threshold_selection"] == "static_hard":
+                        if self.p["bit_mapping"] == "gray":
+                                mapping = {self.p["threshold_levels"][i+1] : i^(i>>1)for i in range(len(self.p["voltage_levels"]))} 
+                        
+                        for threshold1, threshold2 in zip(self.p["threshold_levels"][:-1], self.p["threshold_levels"][1:]):
+                                binary_symbols[np.logical_and(threshold1 <= noisy_symbols, noisy_symbols < threshold2)] = mapping[threshold2]
+                        binary_symbols = np.reshape(binary_symbols, (q,1))
+
+                if self.p["threshold_selection"] == "static_soft":
+                        pass 
+
+                if self.p["threshold_selection"] == "dynamic":
+                        pass
+
+                reshaped_bits = np.unpackbits(binary_symbols, axis=1, count=m, bitorder="little")
+                LLRs[:] = np.reshape(reshaped_bits, self.p["block_length"])
 
 class Channel(Module):
-        
+        """Simulates all noise sources in the FLASH memory cell"""
+
         def add_noise(self, symbols, noisy_symbols):
                 """Adds noise to the signal"""
                 if self.p["voltage_distribution"] == "AWGN":
                         m = int(np.log2(len(self.p["voltage_levels"])))
                         q = int(self.p["block_length"]//m) 
-                        noisy_symbols[:] = symbols + np.random.normal(loc=0, scale=0.2, size=q)
+                        noisy_symbols[:] = symbols + np.random.normal(loc=0, scale=0.0, size=q)
 
 
 class Monitor(Module):
@@ -67,12 +88,12 @@ class Tools:
 
 def init_params():
         return {
-            "block_length": 16000,             #Length of full encoded bit sequence
+            "block_length": 16000,            #Length of full encoded bit sequence
             "bit_mapping": "gray",            #Mapping from bit sequences to voltage levels
             "voltage_levels": [0, 2, 4, 6],   #Target levels for each voltage  
             "voltage_distribution" : "AWGN",  #Model of actual voltage distributions
-            "threshold_selection": "static",  #Thresholding algorithm
-            "threshold_levels" : [5],       #Static thresholding levels
+            "threshold_selection": "static_hard",  #Thresholding algorithm
+            "threshold_levels" : [-inf, 1, 3, 5, inf],   #Static thresholding levels (inlcude +-inf for lazy implmentation)
 
             "eb0_min" : 10,
             "eb0_max" : 12,
@@ -94,7 +115,7 @@ def init_buffers(p):
                 "ref_bits" : np.zeros(p["block_length"], dtype=int),
                 "symbols"  : np.zeros(number_of_symbols),
                 "noisy_symbols" : np.zeros(number_of_symbols),
-                "LLR:s"  : np.zeros(p["block_length"]),
+                "LLRs"  : np.zeros(p["block_length"]),
                 "dec_bits"  : np.zeros(p["block_length"]),
                 }
 
@@ -108,7 +129,7 @@ def main():
         u = init_utils(m)
 
 #loop over SNRs range
-        for ebn0 in np.arange(p["eb0_min"], p["eb0_max"], p["eb0_step"]):
+        #for ebn0 in np.arange(p["eb0_min"], p["eb0_max"], p["eb0_step"]):
                 #compute the current sigma for the channel noise
                 #esn0  = Tools.ebn0_to_esn0 (ebn0, p["R"]) #TODO
                 #sigma = Tools.esn0_to_sigma(esn0)         #TODO
@@ -125,20 +146,27 @@ def main():
                 # run the simulation chain
                 #for i in range(10):
         
-                m["source"].generate(b["ref_bits"])
-                #m.encoder->encode      (b.ref_bits,      b.enc_bits     );
-                m["modem"].modulate(b["ref_bits"], b["symbols"])
-                m["channel"].add_noise(b["symbols"], b["noisy_symbols"])
-
-                plt.figure()
-                plt.subplot(1,1,1)
-                plt.hist(b["noisy_symbols"], 50)
-                plt.hist(b["symbols"], 50)
-                plt.show()
+        m["source"].generate(b["ref_bits"])
+        #m.encoder->encode      (b.ref_bits,      b.enc_bits     );
+        m["modem"].modulate(b["ref_bits"], b["symbols"])
+        m["channel"].add_noise(b["symbols"], b["noisy_symbols"])
+        m["modem"].demodulate(b["noisy_symbols"], b["LLRs"])
 
 
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.hist(b["noisy_symbols"], 50)
+        plt.hist(b["symbols"], 50)                
 
-                #m["modem"].demodulate(b["noisy_symbols"], b["LLRs"])
+        difference = b["ref_bits"] - b["LLRs"]
+        difference = np.reshape(difference, (100,160))
+        plt.subplot(1,2,2)
+        plt.spy(difference)
+        
+        plt.show()
+
+
+
                 #m.decoder->decode_siho (b.LLRs,          b.dec_bits     );
                 #m["monitor"].check_errors(b["dec_bits"], b["ref_bits"])
         
