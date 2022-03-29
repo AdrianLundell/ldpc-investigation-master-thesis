030a6ecb8c420a087d60dc650b07d76803549e08
Fredrik Blomgren <frebl@student.chalmers.se>
2022-03-29 17:34:02 +0200

    Add new channel model that allows different sigmas depending on voltage level

---
+++ aff3ct/include/Module/Channel/AWGN/Channel_AWGN_asymmetric.hpp
@@ -0,0 +1,45 @@
+/*!
+ * \file
+ * \brief Class module::Channel_AWGN_asymmetric.
+ */
+#ifndef CHANNEL_AWGN_ASYMMETRIC_HPP_
+#define CHANNEL_AWGN_ASYMMETRIC_HPP_
+
+#include <memory>
+#include <vector>
+#include <aff3ct.hpp>
+
+namespace aff3ct
+{
+	namespace module
+	{
+		template <typename R = float>
+		class Channel_AWGN_asymmetric : public Channel<R>
+		{
+		private:
+			const bool add_users;
+			std::unique_ptr<tools::Gaussian_gen<R>> noise_generator;
+			tools::Sigma_asymmetric<R> noise_map;
+
+		public:
+			Channel_AWGN_asymmetric(const int N, std::unique_ptr<tools::Gaussian_gen<R>> &&noise_generator,
+						 const bool add_users = false,
+						 const tools::Sigma<R> &total_noise = tools::Sigma<R>((R)1),
+						 const int n_frames = 1);
+
+			explicit Channel_AWGN_asymmetric(const int N, const int seed = 0, const bool add_users = false,
+								  const tools::Sigma<R> &total_noise = tools::Sigma<R>((R)1),
+								  const int n_frames = 1);
+
+			virtual ~Channel_AWGN_asymmetric() = default;
+
+			void add_noise(const R *X_N, R *Y_N, const int frame_id = -1);
+			using Channel<R>::add_noise;
+
+		protected:
+			virtual void check_noise();
+		};
+	}
+}
+
+#endif /* CHANNEL_AWGN_asymmetric_HPP_ */
--- aff3ct/include/aff3ct_extension.hpp
+++ aff3ct/include/aff3ct_extension.hpp
@@ -4,9 +4,11 @@
 #ifndef CHANNEL_TEST_HPP_
 #include <Module/Channel/Test/Channel_Test.hpp>
 #endif
-
-#ifndef CHANNEL_TEST_HPP_
-#include <Module/Modem/FLASH/Modem_FLASH.hpp>
+//#ifndef MODEM_FLASH_HPP_
+//#include <Module/Modem/FLASH/Modem_FLASH.hpp>
+//#endif
+#ifndef CHANNEL_AWGN_ASYMMETRIC_HPP_
+#include <Module/Channel/AWGN/Channel_AWGN_asymmetric.hpp>
 #endif
 
 #endif
