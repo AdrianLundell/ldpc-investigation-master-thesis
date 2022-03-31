eea5fc93dcb604c383bbd3405d958d8d08b71bd3
Fredrik Blomgren <frebl@student.chalmers.se>
2022-03-31 09:38:45 +0200

    Implement functionality that can generate independently random sigmas

--- aff3ct/include/Tools/Algo/Draw_generator/Random_sigma_generator/Random_sigma_generator.hpp
+++ aff3ct/include/Tools/Algo/Draw_generator/Random_sigma_generator/Random_sigma_generator.hpp
@@ -7,32 +7,34 @@
 
 #include <map>
 #include <memory>
+#include <random>
 
 #include "Tools/Algo/Draw_generator/Draw_generator.hpp"
 
 namespace aff3ct
 {
-namespace tools
-{
-template <typename R = float>
-class Random_sigma_generator : public Draw_generator<R>
-{
-private:
-//	std::minstd_rand            rd_engine; // LCG
-	std::mt19937                rd_engine; // Mersenne Twister 19937
-	std::normal_distribution<R> normal_dist;
-public:
-	Random_sigma_generator() = default;
-    explicit Random_sigma_generator(const int seed = 0);
+	namespace tools
+	{
+		template <typename R = float>
+		class Random_sigma_generator : public Draw_generator<R>
+		{
+		private:
+			//	std::minstd_rand            rd_engine; // LCG
+			std::mt19937 rd_engine; // Mersenne Twister 19937
+			std::uniform_int_distribution<R> uniform_int_dist;
 
-	virtual ~Random_sigma_generator() = default;
+		public:
+			Random_sigma_generator() = default;
+			explicit Random_sigma_generator(const int seed = 0);
 
-	virtual void generate( std::map<R,R> &sigma_map, const unsigned size, const R sigma_tot);
+			virtual ~Random_sigma_generator() = default;
 
-    virtual void set_seed(const int seed);
-};
+			virtual void generate(std::map<R, R> &sigma_map, const R sigma_tot, const R min_sigma);
 
-}
+			virtual void set_seed(const int seed);
+		};
+
+	}
 }
 
 #endif /* RANDOM_SIGMA_GENERATOR_HPP_ */
--- aff3ct/src/Tools/Algo/Draw_generator/Random_sigma_generator/Random_sigma_generator.cpp
+++ aff3ct/src/Tools/Algo/Draw_generator/Random_sigma_generator/Random_sigma_generator.cpp
@@ -1,31 +1,51 @@
 #include <aff3ct.hpp>
 #include <aff3ct_extension.hpp>
 
+#include <set>
+
 using namespace aff3ct::tools;
 
 template <typename R>
-Random_sigma_generator<R>
-::Random_sigma_generator(const int seed)
-: Random_sigma_generator<R>()
+Random_sigma_generator<R>::Random_sigma_generator(const int seed)
+	: Random_sigma_generator<R>()
 {
 	this->set_seed(seed);
 }
 
 template <typename R>
-void Random_sigma_generator<R>
-::set_seed(const int seed)
+void Random_sigma_generator<R>::set_seed(const int seed)
 {
 	rd_engine.seed(seed);
 }
 
 template <typename R>
-void Random_sigma_generator<R>
-::generate(std::map<R,R> &sigma_map, const unsigned size, const R sigma_tot)
+void Random_sigma_generator<R>::generate(std::map<R, R> &sigma_map, const R sigma_tot, const R min_sigma)
 {
-	normal_dist = std::normal_distribution<R>(mu, sigma);
+	n_bins = (int)sigma_tot / min_sigma;
+	uniform_int_dist = std::uniform_int_distribution<R>(1, n_bins - 1);
 
-	for (unsigned i = 0; i < length; i++)
-		noise[i] = this->normal_dist(this->rd_engine);
+	std::set<int> rand_bin_levels;
+	unsigned i = 0;
+	unsigned max_i = sigma_map.size() - 1;
+	while (i < max_i)
+	{
+		int rand_bin_level = this->uniform_int_dist(this->rd_engine);
+		if (rand_bin_levels.find(rand_bin_level) == rand_bin_levels.end())
+		{
+			rand_bin_levels.insert(rand_bin_level);
+			i++;
+		}
+	}
+	rand_bin_levels.insert(n_bins);
+
+	auto bin_levels_it = random_bin_levels.begin();
+	int previous_bin = 0;
+	for (auto sigma_it = sigma_map.begin(); sigma_it != sigma_it.end(); sigma_it++)
+	{
+		sigma_it->second = (R)(*bin_levels_it - previous_level) / sigma_tot;
+		previous_bin = *bin_levels_it;
+		bin_levels_it++;
+	}
 }
 
 // ==================================================================================== explicit template instantiation
