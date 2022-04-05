906777c02339ca3d9327cc58bc234829e0c50582
Adrian Lundell <adrian.lundell@gmail.com>
2022-04-05 14:40:50 +0200

    Add 2d interpolation in 2D thresholds

--- aff3ct/include/Tools/Thresholder/Soft/Thresholder_soft.hxx
+++ aff3ct/include/Tools/Thresholder/Soft/Thresholder_soft.hxx
@@ -39,26 +39,50 @@ R Thresholder_soft<R>
 template <typename R>
 void Thresholder_soft<R>
 ::update_thresholds(const tools::Noise<R>& noise){
+	float x = 5.f; //Should be calculated from noise
+	float y = 5.f; //Should be calculated from noise
 
+	std::vector<R>& q11, q12, q21, q22;
+	float x1, x2, y1, y2;
+	//Loop over data to find boundary points assuming increasing values row wise and (snr, ratio) within limits
+	for (auto i = 0; i < data.size(); i++)
+	{
+		if (data[i][0] >= x && data[i][1] >= y)
+		{
+			q22 = data[i];
+			q21 = data[i - 1];
+			q12 = data[i - this->n_snrs];
+			q11 = data[i - this->n_snrs - 1];
 
-	// float sigma_db = noise.get_sigmas();
-	// float sigma_ratio = noise.get_sigmas();
+			x1 = q11[0];
+			x2 = q22[0];
+			y1 = q11[1];
+			y2 = q22[1];
+			break;
+		}
+	}
 
-	// thresholds = data[];
-	// llrs = data[];
+	//Weighted mean interpolation: https://en.wikipedia.org/wiki/Bilinear_interpolation#Weighted_mean
+	float w11, w12, w21, w22, c;
+	c = (x2 - x1)*(y2 - y1);
+	w11 = (x2 - x)*(y2 - y)/c;
+	w12 = (x2 - x)*(y - y1)/c;
+	w21 = (x - x1)*(y2 - y)/c;
+	w22 = (x - x1)*(y - y1)/c;
 
-	// std::vector<R>& x0, x1, x2, x3; 
+	int j;
+	for (auto i = 0; i < this->n_thresholds; i++)
+	{
+		j = i + 2;
+		thresholds[i] = w11*q11[j] + w21*q21[j] + w12*q12[j] * w22*q22[j];	
+	}
 
-	// //Find references to 
-	// for (auto const &x : data)
-	// {
-	// 	if (x[0] >= sigma_db && x[1] >= sigma_ratio)
-	// 	{
-	// 		x1 = x;
-	// 		break;
-	// 	}
-	// 	x0 = x1;
-	// }
+	for (auto i = 0; i < this->n_llrs; i++)
+	{
+		j = i + 2 + this->n_thresholds;
+		llrs[i] = w11*q11[j] + w21*q21[j] + w12*q12[j] * w22*q22[j];	
+	}
+	
 }
 
 template <typename R>
