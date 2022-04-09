bff5d3294a6b99d2a89ff989928649d962f28596
Adrian Lundell <adrian.lundell@gmail.com>
2022-04-09 18:46:13 +0200

    Add noise related imports to aff3ct_extension

--- aff3ct/include/aff3ct_extension.hpp
+++ aff3ct/include/aff3ct_extension.hpp
@@ -5,10 +5,6 @@
 #include <Module/Channel/Test/Channel_Test.hpp>
 #endif
 
-// #ifndef MODEM_FLASH_PAGE_HPP_
-// #include <Module/Modem/flash/Modem_flash.hpp>
-// #endif
-
 #ifndef FLASH_CELL_HPP_
 #include <Tools/Flash/Cell/Flash_cell.hpp>
 #endif
@@ -17,16 +13,16 @@
 #include <Tools/Flash/Reader/Flash_reader.hpp>
 #endif
 
-// #ifndef CHANNEL_AWGN_ASYMMETRIC_HPP_
-// #include <Module/Channel/AWGN/Channel_AWGN_asymmetric.hpp>
-// #endif
+#ifndef CHANNEL_AWGN_ASYMMETRIC_HPP_
+#include <Module/Channel/AWGN/Channel_AWGN_asymmetric.hpp>
+#endif
 
-// ifndef SIGMA_ASYMMETRIC_HPP_
-// include <Tools/Noise/Sigma_asymmetric.hpp>
-// endif
+#ifndef SIGMA_ASYMMETRIC_HPP_
+#include <Tools/Noise/Sigma_asymmetric.hpp>
+#endif
 
-// #ifndef RANDOM_SIGMA_GENERATOR_HPP_
-// #include <Tools/Algo/Draw_generator/Random_sigma_generator/Random_sigma_generator.hpp>
-// #endif
+#ifndef RANDOM_SIGMA_GENERATOR_HPP_
+#include <Tools/Algo/Draw_generator/Random_sigma_generator/Random_sigma_generator.hpp>
+#endif
 
 #endif
