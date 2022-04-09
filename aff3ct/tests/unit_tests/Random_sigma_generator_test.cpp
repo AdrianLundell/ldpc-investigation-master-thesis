e4908a2df58fc42e58c570f7a59b65ade0a2227e
Fredrik Blomgren <frebl@student.chalmers.se>
2022-04-09 18:15:27 +0200

    Create test for Random_sigma_generator

---
+++ aff3ct/tests/unit_tests/Random_sigma_generator_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <aff3ct_extension.hpp>
+#include <math.h>
+
+#include <vector>
+
+using namespace aff3ct;
+class Random_sigma_generator_test : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+    }
+
+    tools::Random_sigma_generator<float> sigma_generator;
+    // void TearDown() override {}
+};
+
+TEST_F(Random_sigma_generator_test, Generate)
+{
+    // Expect two strings not to be equal.
+    float sigma_tot = 3.0;
+    float sigma_min = 0.5;
+    unsigned n_sigmas = 4;
+    std::vector<float> sigmas(n_sigmas, 0.0f);
+
+    sigma_generator.generate(sigmas, sigma_tot, sigma_min);
+
+    for (float sigma : sigmas)
+    {
+        EXPECT_GE(sigma, sigma_min);
+        float sigma_upper_bound = sigma_tot + (n_sigmas - sqrt(n_sigmas));
+        EXPECT_LE(sigma, sigma_upper_bound);
+    }
+}
