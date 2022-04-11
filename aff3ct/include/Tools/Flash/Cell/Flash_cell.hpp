aa9d5e9b612d0075697a00a320cde11a06077c17
Fredrik Blomgren <frebl@student.chalmers.se>
2022-04-11 11:41:05 +0200

    Move .hxx files to source folder to make them executable

--- aff3ct/include/Tools/Flash/Cell/Flash_cell.hpp
+++ aff3ct/include/Tools/Flash/Cell/Flash_cell.hpp
@@ -9,44 +9,53 @@
 
 namespace aff3ct
 {
-namespace tools
-{
-class Flash_cell
-{
-public:
-	explicit Flash_cell(const unsigned cell_type);
+	namespace tools
+	{
+		class Flash_cell
+		{
+		public:
+			explicit Flash_cell(const unsigned cell_type);
 
-	//Reads n_pages of data from Q and returns the mapped level index
-	unsigned get_level_index(const unsigned symbol);
+			// Reads n_pages of data from Q and returns the mapped level index
+			unsigned get_level_index(const unsigned symbol);
 
-	unsigned get_symbol(const unsigned level_index);
-	std::vector<unsigned> get_threshold_indexes(const unsigned page_type);
+			unsigned get_symbol(const unsigned level_index);
+			std::vector<unsigned> get_threshold_indexes(const unsigned page_type);
 
-	unsigned get_n_pages() const;
-	unsigned get_n_levels() const;
-	unsigned get_n_threshold_indexes() const;
+			unsigned get_n_pages() const;
+			unsigned get_n_levels() const;
+			unsigned get_n_threshold_indexes() const;
 
-	enum page_type {lower, upper, extra};
-	enum cell_type {SLC = 2, MLC = 4, TLC = 8};
+			enum page_type
+			{
+				lower,
+				upper,
+				extra
+			};
+			enum cell_type
+			{
+				SLC = 2,
+				MLC = 4,
+				TLC = 8
+			};
 
-private:
-	
-	unsigned n_threshold_indexes;
-	unsigned n_levels;
-	unsigned n_pages;
+		private:
+			unsigned n_threshold_indexes;
+			unsigned n_levels;
+			unsigned n_pages;
 
-	std::vector<std::vector<unsigned>> threshold_indexes;
+			std::vector<std::vector<unsigned>> threshold_indexes;
 
-	std::vector<unsigned> level_index_map;
-	std::vector<unsigned> symbol_map;
-	void init_gray(const unsigned cell_type);
-};
+			std::vector<unsigned> level_index_map;
+			std::vector<unsigned> symbol_map;
+			void init_gray(const unsigned cell_type);
+		};
 
+	}
 }
-}
 
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
-#include "Tools/Flash/Cell/Flash_cell.hxx"
-#endif
+//#ifndef DOXYGEN_SHOULD_SKIP_THIS
+//#include "Tools/Flash/Cell/Flash_cell.hxx"
+//#endif
 
 #endif // FLASH_CELL_HPP_
--- aff3ct/include/Tools/Flash/Reader/Flash_reader.hpp
+++ aff3ct/include/Tools/Flash/Reader/Flash_reader.hpp
@@ -10,52 +10,62 @@
 
 namespace aff3ct
 {
-namespace tools
-{
-template <typename R, typename Q>
-class Flash_reader
-{
-public:
-	explicit Flash_reader(const int page_type, const int read_type, const std::string& fpath);
+	namespace tools
+	{
+		template <typename R, typename Q>
+		class Flash_reader
+		{
+		public:
+			explicit Flash_reader(const int page_type, const int read_type, const std::string fpath);
 
-	typedef aff3ct::module::Channel_AWGN_asymmetric<R> c;
-	void update(c &channel);
-	int get_read_type(); 
-	int get_page_type(); 
+			typedef aff3ct::module::Channel_AWGN_asymmetric<R> c;
+			void update(c &channel);
+			int get_read_type();
+			int get_page_type();
 
-	Q read(const R level, const std::vector<unsigned>& threshold_indexes);
+			Q read(const R level, const std::vector<unsigned> &threshold_indexes);
 
-	enum read_type {hard = 1, soft_single = 3, soft_double = 5};
-	enum page_type {lower = 1, upper = 2, extra = 4};
+			enum read_type
+			{
+				hard = 1,
+				soft_single = 3,
+				soft_double = 5
+			};
+			enum page_type
+			{
+				lower = 1,
+				upper = 2,
+				extra = 4
+			};
 
-private:
-	void _update(const float x, const float y, std::vector<R>& thresholds, std::vector<Q>& bin_values);
+		private:
+			void _update(const float x, const float y, std::vector<R> &thresholds, std::vector<Q> &bin_values);
 
-	void init_data(const std::string& fpath);
-	int count_unique(const std::vector<R>& x);
+			void init_data(const std::string &fpath);
+			int count_unique(const std::vector<R> &x);
 
-	unsigned get_n_thresholds();
-	unsigned get_n_bin_values();
-	R get_threshold(const unsigned threshold_index, const unsigned soft_index);
-	Q get_bin_value(const unsigned threshold_index, const unsigned bin_index);
+			unsigned get_n_thresholds();
+			unsigned get_n_bin_values();
+			R get_threshold(const unsigned threshold_index, const unsigned soft_index);
+			Q get_bin_value(const unsigned threshold_index, const unsigned bin_index);
 
-	std::vector<std::vector<Q>> bin_values;
-	std::vector<std::vector<R>> thresholds;
-	std::vector<std::vector<R>> data;
+			std::vector<std::vector<Q>> bin_values;
+			std::vector<std::vector<R>> thresholds;
+			std::vector<std::vector<R>> data;
 
-	unsigned n_thresholds;
-	unsigned n_bin_values;
-	int n_x;
-	int n_y;
-	int my_page_type; //Change enumeration index
-	int my_read_type;
-};
+			unsigned n_thresholds;
+			unsigned n_bin_values;
+			int n_x;
+			int n_y;
+			int my_page_type; // Change enumeration index
+			int my_read_type;
+		};
 
+	}
 }
-}
 
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
-#include "Tools/Flash/Reader/Flash_reader.hxx"
-#endif
+// #ifndef DOXYGEN_SHOULD_SKIP_THIS
+// #include "Tools/Flash/Reader/Flash_reader.hxx"
+// #endif
 
 #endif // FLASH_READER_HPP_
