94513c564a4a13e158bb2bbafa89322afc41eebd
Fredrik Blomgren <frebl@student.chalmers.se>
2022-04-11 11:52:12 +0200

    Test different seed and add explanations to failed tests

--- aff3ct/tests/unit_tests/Random_sigma_generator_test.cpp
+++ aff3ct/tests/unit_tests/Random_sigma_generator_test.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 using namespace aff3ct;
-class Random_sigma_generator_test : public ::testing::Test
+class RandomSigmaGeneratorTest : public ::testing::Test
 {
 protected:
     void SetUp() override
@@ -16,7 +16,7 @@ protected:
     // void TearDown() override {}
 };
 
-TEST_F(Random_sigma_generator_test, Generate)
+TEST_F(RandomSigmaGeneratorTest, generate)
 {
     // Expect two strings not to be equal.
     float sigma_tot = 3.0;
@@ -24,12 +24,17 @@ TEST_F(Random_sigma_generator_test, Generate)
     unsigned n_sigmas = 4;
     std::vector<float> sigmas(n_sigmas, 0.0f);
 
-    sigma_generator.generate(sigmas, sigma_tot, sigma_min);
-
-    for (float sigma : sigmas)
+    // Test different seeds
+    for (int seed = 0; seed < 20; seed++)
     {
-        EXPECT_GE(sigma, sigma_min);
-        float sigma_upper_bound = sigma_tot + (1 - sqrt(n_sigmas));
-        EXPECT_LE(sigma, sigma_upper_bound);
+        sigma_generator.set_seed(seed);
+        sigma_generator.generate(sigmas, sigma_tot, sigma_min);
+
+        for (float sigma : sigmas)
+        {
+            EXPECT_GE(sigma, sigma_min) << "Each individual sigma must greater or equal to sigma_min";
+            float sigma_upper_bound = sigma_tot + (1 - sqrt(n_sigmas));
+            EXPECT_LE(sigma, sigma_upper_bound) << "Each individual sigma must be smaller than (sigma_tot+(1-sqrt(n_sigmas))";
+        }
     }
 }
