c006ba1286adc08b957f97136584b267a5bf0559
Fredrik Blomgren <frebl@student.chalmers.se>
2022-04-11 11:41:41 +0200

    Comment and uncomment tests

--- aff3ct/tests/unit_tests/Flash_cell_reader_test.cpp
+++ aff3ct/tests/unit_tests/Flash_cell_reader_test.cpp
@@ -1,16 +1,15 @@
-#include <gtest/gtest.h>
-#include <aff3ct_extension.hpp>
+// #include <gtest/gtest.h>
+// #include <aff3ct_extension.hpp>
 
-namespace aff3ct 
-{
-namespace tools
-{
+// namespace aff3ct
+// {
+//     namespace tools
+//     {
 
-TEST(FlashReaderTest, slc)
-{
-    Flash_reader<float, float> slc(Flash_reader<float,float>::lower, Flash_reader<float, float>::hard, "test_data/reader");
-}
+//         TEST(FlashReaderTest, slc)
+//         {
+//             Flash_reader<float, float> slc(Flash_reader<float, float>::lower, Flash_reader<float, float>::hard, "test_data/reader");
+//         }
 
-
-}
-}
+//     }
+// }
