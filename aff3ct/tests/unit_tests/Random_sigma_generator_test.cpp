09e7d3acdbaab08d2debb6d07b66665a6726b1a8
Fredrik Blomgren <frebl@student.chalmers.se>
2022-04-12 09:53:35 +0200

    Change message of failed test

--- aff3ct/tests/unit_tests/Random_sigma_generator_test.cpp
+++ aff3ct/tests/unit_tests/Random_sigma_generator_test.cpp
@@ -34,7 +34,7 @@ TEST_F(RandomSigmaGeneratorTest, generate)
         {
             EXPECT_GE(sigma, sigma_min) << "Each individual sigma must greater or equal to sigma_min";
             float sigma_upper_bound = sigma_tot + sigma_min * (1 - sqrt(n_sigmas));
-            EXPECT_LE(sigma, sigma_upper_bound) << "Each individual sigma must be smaller than (sigma_tot+(1-sqrt(n_sigmas))";
+            EXPECT_LE(sigma, sigma_upper_bound) << "Each individual sigma must be smaller than (sigma_tot+sigma_min*(1-sqrt(n_sigmas))";
         }
     }
 }
