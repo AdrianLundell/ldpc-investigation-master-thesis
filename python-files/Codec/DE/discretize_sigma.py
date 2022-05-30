cff2a3d70e02f540246cfc22e3e36dc9b731e3d2
Adrian Lundell <adrian.lundell@gmail.com>
2022-05-30 10:26:59 +0200

    Set up files for full density evolution

--- python-files/Codec/DE/discretize_sigma.py
+++ python-files/Codec/DE/discretize_sigma.py
@@ -1,4 +1,13 @@
-import optimal_thresholds as t
+#%%
+import sys 
+import os 
+sys.path.insert(1, os.path.join(sys.path[0], '../..'))
+
+import Modem.optimal_thresholds as optimize
+import Analysis.utils as utils
+
+import numpy as np 
+import matplotlib.pyplot as plt 
 
 #%%
 
@@ -13,9 +22,9 @@ def compute_pdf(sigma, ratio, n_thresholds, n_grid, llr_max):
     """Returns values and bin numbers of the pdf defining the A-BIAWGN (sigma, ratio) 
        discretized by n_thresholds and quantized to the grid of n_grid points over (-llr_max, llr_max)"""
 
-    thresholds = t.optimize_threhsolds(sigma)
-    llrs = t.LLR(thresholds)
-    real_sigma = calculate_sigma(sigma, ratio)
+    thresholds = optimize.optimize_threhsolds(sigma)
+    llrs = optimize.LLR(thresholds)
+    real_sigma = utils.rber_to_sigma(sigma, ratio)
 
     step = 2*llr_max / n_grid
     bins = np.round(thresholds / step)
