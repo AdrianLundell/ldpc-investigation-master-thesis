46b2e32894ca7e74932afac7f4d0e8e2f820b5ce
Adrian Lundell <adrian.lundell@gmail.com>
2022-06-08 13:31:13 +0200

    bec validation

--- python-files/Codec/DE/density_evolution.py
+++ python-files/Codec/DE/density_evolution.py
@@ -7,134 +7,50 @@ https://arxiv.org/pdf/2001.01249.pdf
 
 """
 
+import time
+from unicodedata import name 
 import de_methods
 import generate_distributions
 
 import numpy as np 
 import matplotlib.pyplot as plt
+np.seterr(divide='ignore')
 
-#%%
-def density_evolution(p0_pdf, f_grid, g_grid, r, l, n_iter = 50, tol = 1e-6):
+def symmetric_density_evolution(cdf, f_grid, g_grid, rho_coeffs, lambda_coeffs, n_iter = 50, tol = 1e-6, plot = True):
+    if plot: 
+        fig, axes = plt.subplots(1,2)
 
-    # fig, axes = plt.subplots(1,2)
-    p0 = symmetric_de.to_cdf(p0_pdf)
-    pl = p0
+    pl = cdf 
+    p0 = cdf
+    pl_old = 0
+    i = 0
+    diff = np.inf
+    error = np.inf
+    while (diff > 1e-8) and (error > tol) and (i < n_iter):
+        x1 = de_methods.gamma(pl, f_grid, g_grid)
+        x2 = de_methods.rho(x1, rho_coeffs)
+        x3 = de_methods.gamma_inv(x2, f_grid, g_grid)
+        x4 = de_methods.lambd(x3, lambda_coeffs)
+        pl = de_methods.conv(x4, p0)
 
-    for i in range(n_iter):
-        x1 = symmetric_de.gamma(pl, f_grid, g_grid)
+        diff = sum((pl_old - pl)**2)/np.where((pl>0) & (pl <1))[0].size
+        pl_old = pl
+
+        zero_index = pl.size//2 + 5
+        error = pl[zero_index]
         
-        x2 = symmetric_de.rho(x1, r)
-        x3 = symmetric_de.gamma_inv(x2, f_grid, g_grid)
-        x4 = symmetric_de.lambd(x3, l)
-        pl = symmetric_de.conv(x4, p0)
+        if plot:
+            axes[0].plot(pl)
+            axes[1].scatter(i, error)
 
-        error = pl[pl.size//2]
-        # axes[0].plot(pl)
-        # axes[1].scatter(i, error)
+        i += 1
 
-        if error < tol:
-            return True
-
-    # plt.show()
-    return False
-
-#%% Single run
-n_grid = 8192
-sigma, p0, bins = discretize_sigma.compute_pdf(0.01, 0.5, n_grid, 30)
-f_grid, g_grid, pdf = discretize_sigma.create_pdf(p0, bins, n_grid)
-cdf = symmetric_de.to_cdf(pdf)
-lambda_coeffs = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1])
-rho_coeffs = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1])
-
-density_evolution(pdf, f_grid, g_grid, rho_coeffs, lambda_coeffs, 50)
-
-#%% Many runs
-import time 
-
-t0 = time.time()
-n_grid = 8192
-
-rho_coeffs = np.array([0, 
-1.1203e-04,
-0.0023,
-0.0056,
-0.0092,
-0.0126,
-0.0153,
-0.0172,
-0.0185,
-0.0193,
-0.0203,
-0.0220,
-0.0235,
-0.0253,
-0.0267,
-0.0251,
-0.0240,
-0.0195,
-6.4078e-04,
-0.7129])
-
-lambda_coeffs = np.array([0, 
-0.9464,
-0.0325,
-0.0033,
-0.0022,
-0.0018,
-0.0015,
-0.0013,
-0.0012,
-0.0011,
-0.0010,
-9.8768e-04,
-9.4196e-04,
-9.0299e-04,
-8.6939e-04,
-8.4013e-04,
-8.1443e-04,
-7.9168e-04,
-7.7141e-04,
-7.5322e-04])
-
-for rber in np.linspace(0.001, 0.1):
-    sigma, p0, bins = discretize_sigma.compute_pdf(rber, 0.5, n_grid, 30)
-    f_grid, g_grid, pdf = discretize_sigma.create_pdf(p0, bins, n_grid)
-    cdf = symmetric_de.to_cdf(pdf)
-    
-    converged = density_evolution(pdf, f_grid, g_grid, rho_coeffs, lambda_coeffs, 50)
-    
-    if not converged:
-        print(f"RBER : {rber}")
-        break
-
-
-print(f"{time.time() - t0} seconds")
-
-# %%
-
-def density_evolution(p0_pdf, f_grid, g_grid, n_iter = 50):
-
-    pl = to_cdf(p0_pdf)
-
-    for l in range(n_iter):
-        plt.plot(pl)
-        plt.show()
-        x1 = gamma(pl, f_grid, g_grid)
-        plt.plot(g_grid, x1[0,:])
-        plt.show()
-        plt.plot(g_grid, x1[1,:])
-        plt.show()
-        
-        x2 = rho(x1)
-        x3 = gamma_inv(x2, f_grid, g_grid)
-        x4 = lambd(x3)
-        pl = conv(x4, )
-
+    if plot:
         plt.show()
 
+    return np.round(error, int(-np.log10(tol)))
 
-#%% Bisection search
-def bisection_search(min, max, eval, tol = 1e-5):
+def bisection_search(min, max, eval, tol = 1e-4):
     while max - min > tol:
         x = (min + max)/2 
         result = eval(x)
@@ -147,3 +63,25 @@ def bisection_search(min, max, eval, tol = 1e-5):
             raise Exception
 
     return (min + max)/2
+
+#%%
+if __name__ == "main":
+    t0 = time.time()
+    n_grid = 8192
+    rho_coeffs = np.array([0,1.1203e-04, 0.0023, 0.0056, 0.0092,0.0126,0.0153,0.0172,0.0185,0.0193,0.0203,0.0220,0.0235,0.0253,0.0267,0.0251,0.0240,0.0195,6.4078e-04, 0.7129])
+    lambda_coeffs = np.array([0,0.9464,0.0325,0.0033,0.0022,0.0018,0.0015,0.0013,0.0012,0.0011,0.0010,9.8768e-04,9.4196e-04,9.0299e-04,8.6939e-04,8.4013e-04,8.1443e-04,7.9168e-04,7.7141e-04,7.5322e-04])
+
+    min = 0.01
+    max = 0.1
+
+    def eval(x):
+        sigma, p0, bins = generate_distributions.compute_pdf(x, 0.5, n_grid, 30)
+        f_grid, g_grid, pdf = generate_distributions.create_pdf(p0, bins, n_grid)
+        cdf = de_methods.to_cdf(pdf)
+
+        return symmetric_density_evolution(cdf, f_grid, g_grid, rho_coeffs, lambda_coeffs, plot = False)
+
+    result = bisection_search(min, max, eval)
+
+    print(result)
+    print(time.time()-t0)
--- python-files/Codec/DE/validation_bec.py
+++ python-files/Codec/DE/validation_bec.py
@@ -1,4 +1,46 @@
 """
 This file validates the implementation of the density evolution by replicating the results of the simple bec-channel expression.
-"""
 
+Expected 
+(3, 6) 0.4294
+(4, 8) 0.3834
+(5, 10) 0.3415
+(6, 12) 0.3074
+(7, 14) 0.2798
+"""
+import time 
+import numpy as np 
+import density_evolution 
+import matplotlib.pyplot as plt
+
+t0 = time.time()
+n_grid = 8192
+
+ensamble = (4, 8)
+lambda_coeffs = [0]*ensamble[0]
+lambda_coeffs[-1] = 1
+rho_coeffs = [0]*ensamble[1]
+rho_coeffs[-1] = 1
+
+min = 0.1
+max = 1
+
+f_grid = np.linspace(-30, 30, n_grid, endpoint = False)
+f_step = abs(f_grid[1] - f_grid[0])
+max_val = -np.log(np.tanh(f_step))
+g_grid = np.linspace(0, max_val, n_grid)
+
+def eval(x):
+    cdf = np.zeros(n_grid)
+    cdf[n_grid//2:] = x
+    cdf[-1] = 1
+  #  plt.plot(cdf)
+ #   plt.show()
+
+    return density_evolution.symmetric_density_evolution(cdf, f_grid, g_grid, rho_coeffs, lambda_coeffs, plot = True)
+
+result = density_evolution.bisection_search(min, max, eval)
+
+print(result)
+print(time.time()-t0)
+# %%
