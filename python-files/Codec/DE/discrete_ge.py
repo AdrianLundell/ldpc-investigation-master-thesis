d7026adf3f5876f78161991e38b83807eac87bd2
Adrian Lundell <adrian.lundell@gmail.com>
2022-06-14 12:33:40 +0200

    Further work on ge

--- python-files/Codec/DE/discrete_ge.py
+++ python-files/Codec/DE/discrete_ge.py
@@ -7,9 +7,8 @@ n_cn = 9//3
 n_vn = 73//3
 n_pop = 50
 
-
-def init_population(n_pop):
-    population = np.zeros((n_pop, n_cn, n_vn))
+def init_population(n_pop, n_cn, n_vn):
+    population = np.zeros((n_pop, n_cn, n_vn), int)
     for i in range(n_pop):
         for j in range(n_vn):   
             k = np.random.choice(n_cn, 2, replace = False)
@@ -17,27 +16,22 @@ def init_population(n_pop):
     
     return population
 
-def rho_coeffs(i):
-    pass
-
-def lam_coeffs(i):
-    pass
-
 def vertical_crossover(i1, i2):
     crossover_point = np.random.randint(i1[0,:].size)
-    i1_new = np.vstack((i1[:,:crossover_point], i2[:,crossover_point:]))
-    i2_new = np.vstack((i1[:,crossover_point:], i2[:,:crossover_point]))
+    i1_new = np.hstack((i1[:,:crossover_point], i2[:,crossover_point:]))
+    i2_new = np.hstack((i1[:,crossover_point:], i2[:,:crossover_point]))
     return i1_new, i2_new
 
 def horizontal_crossover(i1, i2):
     crossover_point = np.random.randint(i1[:,0].size)
-    i1_new = np.hstack((i1[:crossover_point,:], i2[crossover_point:,:]))
-    i2_new = np.hstack((i1[crossover_point:,:], i2[:crossover_point,:]))
+    i1_new = np.vstack((i1[:crossover_point,:], i2[crossover_point:,:]))
+    i2_new = np.vstack((i1[crossover_point:,:], i2[:crossover_point,:]))
     return i1_new, i2_new
 
 def mutation(i):
-    mutation_index = np.random.randint(i.size)
-    i[mutation_index] = not i[mutation_index]
+    mutation_i = np.random.randint(0, i[:,0].size)
+    mutation_j = np.random.randint(0, i[0,:].size)
+    i[mutation_i,mutation_j] = not i[mutation_i,mutation_j]
     return i
 
 def tournament(population, fitness, n_competitiors = 3):
@@ -45,7 +39,8 @@ def tournament(population, fitness, n_competitiors = 3):
     competitors_fitness = fitness[competitors_index] 
     winner_index = competitors_index[np.argmax(competitors_fitness)]
     i_new = population[winner_index, :,:]
-    return i_new
+    fitness_new = fitness[winner_index]
+    return i_new, fitness_new
 
 def evaluate(i):
     cn_degrees = np.sum(i, 0)
@@ -56,51 +51,100 @@ def evaluate(i):
 
     if condition1 and condition2:
         rho_node = np.bincount(vn_degrees)
-        rho_edge = np.arange(1,len(rho_node)) * rho_node / np.sum(np.arange(1,len(rho_node)) * rho_node)
+        rho_edge = np.arange(1,len(rho_node)+1) * rho_node / np.sum(np.arange(1,len(rho_node)+1) * rho_node)
         lam_node = np.bincount(cn_degrees)
-        rho_edge = np.arange(1,len(lam_node)) * lam_node / np.sum(np.arange(1,len(lam_node)) * lam_node)
+        rho_edge = np.arange(1,len(lam_node)+1) * lam_node / np.sum(np.arange(1,len(lam_node)+1) * lam_node)
 
-        fitness = 
+        fitness = np.max(vn_degrees)
         return fitness
     else:
         return -1
 
 #%%
-n_pop = 1
-n_generations = 1
-p_vertical = 0.5
-p_horizontal = 0.5 
-p_mutation = 0.01
+fname = None
 
-population = init_population(n_pop)
-fitness = np.zeros(n_pop)
+if fname is None:
+    population = init_population(n_pop, n_cn, n_vn)
+    fitness = np.full(n_pop, -np.inf)
+    n_pop = 50
+    n_generations = 100
+    n_vn = 73
+    n_cn = 9
+    p_vertical = 0.5
+    p_horizontal = 0.5 
+    p_mutation = 0.01
+
+    fname = "test_ge.npz"
+else:
+    data = np.load(fname)
+    
+    population = init_population(n_pop, n_cn, n_vn)
+    fitness = np.full(n_pop, -np.inf)
+    
+    n_pop = data["n_pop"]
+    n_generations = data["n_generations"]
+    n_vn = data["n_vn"]
+    n_cn = data["n_cn"]
+    
+    p_vertical = data["p_vertical"]
+    p_horizontal = data["p_horizontal"]
+    p_mutation = data["p_mutation"]
+
+    fname = fname
+
+print(f"""
+===================================================================
+Optimization
+===================================================================
+""")
 
 try:
     for i in range(n_generations):
-        #Evaluate
+        
+        #Evaluate new individuals
         for j in range(n_pop):
-            fitness[j] = evaluate(population[j,:,:])
+            if fitness[j] == -np.inf:
+                fitness[j] = evaluate(population[j,:,:])
 
         #Select with elitism
-        population_new = np.zeros(population.shape)
+        population_new = np.zeros(population.shape, int)
+        fitness_new = np.full(fitness.shape, -np.inf)
         population_new[0] = population[np.argmax(fitness)]
+        fitness_new[0] = fitness[np.argmax(fitness)]
         for j in range(1,n_pop):
-            population_new[j] = evaluate(population[j,:,:])   
+            population_new[j], fitness_new[j] = tournament(population, fitness) 
         population = population_new
+        fitness = fitness_new
 
         #Crossover
         for j in range(1, n_pop, 2):
             if np.random.rand() < p_vertical:
                 population[j], population[j+1] = vertical_crossover(population[j], population[j+1])
+                fitness[j], fitness[j+1] = -np.inf, -np.inf
             if np.random.rand() < p_horizontal:
                 population[j], population[j+1] = horizontal_crossover(population[j], population[j+1])
+                fitness[j], fitness[j+1] = -np.inf, -np.inf
 
         #Mutation
         for j in range(1, n_pop):
             if np.random.rand():
                 population[j] = mutation(population[j])
+                fitness[j] = -np.inf
 
+        status = f"Generation {i}/{n_generations} Best/median/mean fitness: {np.max(fitness)}"
+        print(status, end='\r', flush=True)
 
 finally:
-    with open('x_best.npy', 'wb') as f:
-        np.save(f, x_best)
+    with open(fname, 'wb') as f:
+        np.savez(f, population = population, 
+                    fitness = fitness, 
+                    
+                    n_pop = np.array(n_pop),
+                    n_generations = np.array(n_generations),
+                    n_vn = np.array(n_vn),
+                    n_cn = np.array(n_cn),
+                    
+                    p_vertical = np.array(p_vertical),
+                    p_horizontal = np.array(p_horizontal),
+                    p_mutation = np.array(p_mutation))
+# %%
