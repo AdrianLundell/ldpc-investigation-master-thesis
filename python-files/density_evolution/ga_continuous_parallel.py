035e9f57be0e20b651ec977deac84621a6f6ffb6
Fredrik Blomgren <frebl@student.chalmers.se>
2022-06-19 22:16:51 +0200

    Parallelize algorithms

---
+++ python-files/density_evolution/ga_continuous_parallel.py
@@ -0,0 +1,315 @@
+# %% Imports and variables
+from time import process_time
+import numpy as np
+from scipy.optimize import linprog
+from scipy.linalg import null_space
+import de_utils as de_u
+import random
+from multiprocessing import Pool, shared_memory
+from multiprocessing.sharedctypes import Value
+
+
+from config import cfg
+cfg_de = cfg.get('density_evolution')
+cfg_cont = cfg_de.get('ga_continuous')
+run_id = cfg.get("run_id")
+n_processes = cfg.get("n_processes")
+
+
+def start_point(R, dv, dc):
+    # lam and rho are assumed to begin at degree 2
+    k = 1/np.arange(2, dc+1)
+    i = 1/np.arange(2, dv+1)
+
+    # C is the constraint matrix.
+    # 1st row: code rate constraint
+    # 2nd row: coefficients of rho should sum to 1
+    # 3rd row: coefficients of lam should sum to 1
+    # lb: coefficients of rho and lam should be positive (>0)
+    c1 = np.concatenate((k, -(1-R)*i))
+    c2 = np.concatenate((np.ones(dc-1), np.zeros(dv-1)))
+    c3 = np.concatenate((np.zeros(dc-1), np.ones(dv-1)))
+    C = np.stack((c1, c2, c3))
+
+    d = np.array([[0], [1], [1]])
+    lb = np.zeros((dv+dc-2, 1))
+    f = np.ones((dv+dc-2, 1))
+
+    sol = linprog(f, A_ub=None, b_ub=None, A_eq=C,
+                  b_eq=d, method='interior-point')
+    x_0 = sol.x
+
+    return C, x_0
+
+
+def add_one_degree(C_c, x_0, dc):
+
+    # %% Add degree 1
+    x_0 = np.concatenate(([0], x_0[:dc-1], [0], x_0[dc-1:]))
+    D = np.size(C_c, 1)
+    z_vec = np.zeros((1, D))
+    C_c = np.vstack((z_vec, C_c[:dc-1, :], z_vec, C_c[dc-1:, :]))
+
+    return C_c, x_0
+
+# Differential evolution help functions
+
+
+def compute_x(x_0, C_c, eta):
+    return x_0 + np.matmul(C_c, eta)
+
+
+def poly_eval(p, x):
+    exp = np.arange(0, p.size)
+    return p*x**exp
+
+
+def compute_fitness(x, dc):
+    fitness = 0
+    in_domain = True
+    for xi in x:
+        if xi < 0:
+            fitness += -100 + 10*xi
+            in_domain = False
+
+    if fitness == 0:
+        rho = x[:dc]
+        lam = x[dc:]
+        min = cfg_de.get("min_rber")
+        max = cfg_de.get("max_rber")
+        result = de_u.bisection_search(
+            min, max, rho, lam)
+        fitness = 100*result
+
+    return fitness, in_domain
+
+# Function that is parallelized through Pool in multiprocessing
+def de_individual(i):
+    global eta_name, u_name, fit_name, dom_name, eta_shape, u_shape, fit_shape, dom_shape, Np, Cr, F, C_c, x_0, g_idx, dc
+    D = np.size(C_c,1)
+    shm_eta = shared_memory.SharedMemory(name = eta_name)
+    shm_u = shared_memory.SharedMemory(name=u_name)
+    shm_fit = shared_memory.SharedMemory(name=fit_name)
+    shm_dom = shared_memory.SharedMemory(name=dom_name)
+
+    eta = np.ndarray(eta_shape, dtype=np.float64, buffer=shm_eta.buf)
+    u = np.ndarray(u_shape, dtype=np.float64, buffer=shm_u.buf)
+    fitness = np.ndarray(fit_shape, dtype=np.float64, buffer=shm_fit.buf)
+    domain = np.ndarray(dom_shape, dtype=bool, buffer=shm_dom.buf)
+
+    g = g_idx.value
+
+    # Generate selection parameters according to algorithm
+    r0 = random.randint(0, Np-1)
+    r1 = random.randint(0, Np-1)
+    r2 = random.randint(0, Np-1)
+    jrand = random.randint(0, D-1)
+    while (r0 == i):
+        r0 = random.randint(0, Np-1)
+    while (r1 == i or r1 == r0):
+        r1 = random.randint(0, Np-1)
+    while (r2 == i or r2 == r0 or r2 == r1):
+        r2 = random.randint(0, Np-1)
+
+    # Mutation and cross over
+    cr_arr = np.random.rand(D)
+    for j in range(D):
+        if (cr_arr[j] < Cr or j == jrand):
+            u[j, i] = eta[j, r0] + F * \
+                (eta[j, r1]-eta[j, r2])
+
+    # Selection
+    if g_idx.value == 0:
+        x = compute_x(x_0, C_c, eta[:, i])
+        fit, in_domain = compute_fitness(x, dc)
+        fitness[g, i] = fit
+        domain[i] = in_domain
+    else:
+        x = compute_x(x_0, C_c, u[:, i])
+        fit, in_domain = compute_fitness(x, dc)
+
+        if fit > fitness[g-1, i]:
+            eta[:, i] = u[:, i]
+            fitness[g, i] = fit
+            domain[i] = in_domain
+        else:
+            fitness[g, i] = fitness[g-1, i]
+
+    shm_eta.close()
+    shm_u.close()
+    shm_fit.close()
+    shm_dom.close()
+
+
+# Used for initializeing multiprocessing
+def pool_initializer():
+    global eta_name, u_name, fit_name, dom_name, eta_shape, u_shape, fit_shape, dom_shape, Np, Cr, F, C_c, x_0, g_idx, dc
+
+
+
+def differential_evolution(C_c, x_0, dc):
+    global eta_name, u_name, fit_name, dom_name, eta_shape, u_shape, fit_shape, dom_shape, Np, Cr, F, g_idx
+    # Np: number of individuals in population
+    # gens: Number of generations. Around 5000
+    # F: mutation variable. Usually in interval [0.1,1].
+    # Cr: recombination probability, [0,1].
+
+    Np = cfg_de.get('Np')
+    gens = cfg_de.get('generations')
+    F = cfg_cont.get('F')
+    Cr = cfg_cont.get('Cr')
+    D = np.size(C_c, 1)
+    load_population = cfg_de.get("load_population")
+    de_u.save_params()
+
+    code_rate = cfg_cont.get("R")
+    dv = cfg_cont.get("dv")
+    dc = cfg_cont.get("dc")
+
+    print_terminal = cfg_de.get("print_terminal")
+    header = f"""
+    Running ga_continuous.py
+    ===================================================================
+    Optimizing code ensamble through differential evolution algorithm.
+    Code rate: {code_rate:.2f}.
+    Number of individuals: {Np}.
+    Number of generations: {gens}.
+    Max variable node degree: {dv}.
+    Max check node degree: {dc}.
+    Parameter settings: F = {F}, Cr = {Cr}
+    -------------------------------------------------------------------
+    """
+    if print_terminal:
+        print(header)
+    else:
+        de_u.log(header,'w')
+
+    # x = x_0 + C_c*eta
+    # Initialize population
+    if load_population:
+        fname = "data/" + run_id + ".npz"
+        data = np.load(fname)
+        eta_Np = data["population"]
+        fitness_Np = data["fitness"]
+        g_start = int(data["generation"][0])+1
+        dim_0 = np.size(fitness_Np, axis=0)
+        if gens != np.size(fitness_Np, axis=0):
+            fitness_Np_new = np.zeros((gens, Np))
+            fitness_Np_new[:dim_0] = fitness_Np
+            fitness_Np = fitness_Np_new
+    else:
+        eta_Np = np.random.rand(D, Np)
+        fitness_Np = np.zeros((gens,Np))
+        g_start = 0
+
+    domain_Np = np.full(Np, False)
+
+    # Initialize shared_memory in multiprocessing
+    shm_eta = shared_memory.SharedMemory(create=True, size=eta_Np.nbytes)
+    shm_u = shared_memory.SharedMemory(create=True, size=eta_Np.nbytes)
+    shm_fit = shared_memory.SharedMemory(create=True, size=fitness_Np.nbytes)
+    shm_dom = shared_memory.SharedMemory(create=True, size=domain_Np.nbytes)
+    
+    eta = np.ndarray(eta_Np.shape, dtype=np.float64, buffer=shm_eta.buf)
+    u = np.ndarray(eta_Np.shape, dtype=np.float64, buffer=shm_u.buf)
+    fitness = np.ndarray(fitness_Np.shape, dtype=np.float64, buffer=shm_fit.buf)
+    domain = np.ndarray(domain_Np.shape, dtype=bool, buffer=shm_dom.buf)
+
+    eta[:] = eta_Np[:]
+    u[:] = eta_Np[:]
+    fitness[:] = fitness_Np[:]
+    domain[:] = domain_Np[:]
+
+    eta_name = shm_eta.name
+    u_name = shm_u.name
+    fit_name =  shm_fit.name
+    dom_name = shm_dom.name
+    
+    eta_shape = eta.shape
+    u_shape = u.shape 
+    fit_shape = fitness.shape
+    dom_shape = domain.shape
+
+    g_idx = Value('i', g_start)
+
+    # Use Pool for parallel computing
+    pool = Pool(processes=n_processes, initializer=pool_initializer)
+    pop_idx = range(Np)
+    
+    try:
+        # Optimize population over generations
+        t1_start = process_time()
+        for g in range(g_start,gens):
+
+            u[:] = np.copy(eta)
+            pool.map(de_individual, pop_idx)
+            # Equivalent to:
+            #for i in range(Np):
+            #    de_individual(i)
+            g_idx.value += 1
+            
+            ave_fitness = np.sum(fitness[g,:])/Np
+
+            # Write current status
+            
+            n_domain = domain.sum()
+            if n_domain != 0:
+                rber_max = np.max(fitness[g,domain])
+                rber_min = np.min(fitness[g,domain])
+            else:
+                rber_max = 0
+                rber_min = 0
+            t1_stop = process_time()
+            status = f"{g} generations completed.  Average fitness:{ave_fitness:.2f}.  Individuals in domain: {n_domain}. Max RBER: {rber_max:.2f}. Min RBER: {rber_min:.2f} "
+            if print_terminal:    
+                print(status, end='\r', flush=True)
+            else:
+                de_u.log(status,'a')
+
+            if g % int(cfg_de.get("save_interval")) == 0:
+                de_u.save_population(eta, fitness, g)
+
+        status = f"""
+    -------------------------------------------------------------------
+    Finished!
+    ===================================================================
+            """
+        if print_terminal:
+            print(status)
+        else:
+            de_u.log(status,'a')
+    finally:
+        de_u.save_population(eta_Np, fitness_Np, g)
+        shm_eta.close()
+        shm_u.close()
+        shm_fit.close()
+        shm_dom.close()
+        shm_eta.unlink()
+        shm_u.unlink()
+        shm_fit.unlink()
+        shm_dom.unlink()
+
+
+def ga_continous_parallel():
+    global C_c, x_0, dc
+
+    R = cfg_cont.get('R')
+    dv = cfg_cont.get('dv')
+    dc = cfg_cont.get('dc')
+
+    # x = [rho;lam]
+    # 1. Compute an initial start point
+    C, x_0 = start_point(R, dv, dc)
+
+    # 2. Find the complement of constraint matrix.
+    # This gives us the allowed directions to step around in
+    C_c = null_space(C)
+
+    # 3. Add degree 1 to x_0 and C_c matrix
+    C_c, x_0 = add_one_degree(C_c, x_0, dc)
+
+    # 4. Perform optimization through differential evolution
+    differential_evolution(C_c, x_0, dc)
+
+# %%
+
---
+++ python-files/density_evolution/ga_discrete_parallel.py
@@ -0,0 +1,233 @@
+import numpy as np
+import de_utils as de_u
+import matplotlib.pyplot as plt
+from multiprocessing import Pool, shared_memory, Value
+
+from config import cfg
+cfg_de = cfg.get('density_evolution')
+cfg_disc = cfg_de.get('ga_discrete')
+run_id = cfg.get("run_id")
+n_processes = cfg.get("n_processes")
+
+
+def init_population(n_pop, n_cn, n_vn):
+    population = np.zeros((n_pop, n_cn, n_vn), int)
+    for i in range(n_pop):
+        for j in range(n_vn):
+            k = np.random.choice(n_cn, 2, replace=False)
+            population[i, k, j] = 1
+
+    return population
+
+
+def vertical_crossover(i1, i2):
+    crossover_point = np.random.randint(i1[0, :].size)
+    i1_new = np.hstack((i1[:, :crossover_point], i2[:, crossover_point:]))
+    i2_new = np.hstack((i1[:, crossover_point:], i2[:, :crossover_point]))
+    return i1_new, i2_new
+
+
+def horizontal_crossover(i1, i2):
+    crossover_point = np.random.randint(i1[:, 0].size)
+    i1_new = np.vstack((i1[:crossover_point, :], i2[crossover_point:, :]))
+    i2_new = np.vstack((i1[crossover_point:, :], i2[:crossover_point, :]))
+    return i1_new, i2_new
+
+
+def mutation(i):
+    mutation_i = np.random.randint(0, i[:, 0].size)
+    mutation_j = np.random.randint(0, i[0, :].size)
+    i[mutation_i, mutation_j] = not i[mutation_i, mutation_j]
+    return i
+
+
+def tournament(population, fitness, n_competitiors=3):
+    n_pop = cfg_de.get("Np")
+    competitors_index = np.random.choice(n_pop, n_competitiors, replace=False)
+    competitors_fitness = fitness[competitors_index]
+    winner_index = competitors_index[np.argmax(competitors_fitness)]
+    i_new = population[winner_index, :, :]
+    fitness_new = fitness[winner_index]
+    return i_new, fitness_new
+
+
+def evaluate(j):
+    global pop_name, fit_name, pop_shape, fit_shape, i_idx
+
+    shm_pop = shared_memory.SharedMemory(name=pop_name)
+    shm_fit = shared_memory.SharedMemory(name=fit_name)
+    pop = np.ndarray(pop_shape, dtype=np.int64, buffer=shm_pop.buf)
+    fit = np.ndarray(fit_shape, dtype=np.float64, buffer=shm_fit.buf)
+    i = i_idx.value
+
+    if fit[i, j] == -np.inf:
+        cn_degrees = np.sum(pop[j, :, :], 0)
+        vn_degrees = np.sum(pop[j, :, :], 1)
+
+        condition1 = np.all(cn_degrees >= 2) and np.all(vn_degrees >= 2)
+        condition2 = np.all(cn_degrees <= 25)
+
+        if condition1 and condition2:
+            rho_node = np.bincount(vn_degrees)[1:]
+            rho_edge = np.arange(1, len(rho_node)+1) * rho_node / \
+                np.sum(np.arange(1, len(rho_node)+1) * rho_node)
+            lam_node = np.bincount(cn_degrees)[1:]
+            lam_edge = np.arange(1, len(lam_node)+1) * lam_node / \
+                np.sum(np.arange(1, len(lam_node)+1) * lam_node)
+
+            min = cfg_de.get("min_rber")
+            max = cfg_de.get("max_rber")
+            fitness = de_u.bisection_search(min, max, rho_edge, lam_edge)
+            fit[i,j] = fitness*100
+
+        else:
+            fit[i, j]  = -100
+    shm_pop.close()
+    shm_fit.close()
+
+def pool_initializer():
+    global pop_name, fit_name, pop_shape, i_idx
+
+def ga_discrete_parallel():
+    global pop_name, fit_name, pop_shape, fit_shape, i_idx
+    load_population = cfg_de.get("load_population")
+
+    n_pop = cfg_de.get("Np")
+    n_generations = cfg_de.get("generations")
+    n_vn = cfg_disc.get("n_vn")
+    n_cn = cfg_disc.get("n_cn")
+    p_vertical = cfg_disc.get("p_vertical")
+    p_horizontal = cfg_disc.get("p_horizontal")
+    p_mutation = cfg_disc.get("p_mutation")
+    print_terminal = cfg_de.get("print_terminal")
+
+    de_u.save_params()
+
+    if load_population:
+        fname = "data/" + run_id + ".npz"
+        data = np.load(fname)
+        population = data["population"]
+        fitness = data["fitness"]
+        i_start = int(data["generation"][0]) + 1
+        dim_0 = np.size(fitness, axis=0)
+        if n_generations != np.size(fitness, axis=0):
+            fitness_new = np.zeros((n_generations, n_generations))
+            fitness_new[:dim_0] = fitness
+            fitness = fitness_new
+    else:
+        population = init_population(n_pop, n_cn, n_vn)
+        fitness = np.full((n_generations, n_pop), -np.inf)
+        i_start = 0
+
+    code_rate = (n_vn-n_cn)/n_vn
+    
+    header = f"""
+    Running ga_disrete.py
+    ===================================================================
+    Optimizing {n_cn}x{n_vn} protograph.
+    Code rate: {code_rate:.2f}.
+    Number of individuals: {n_pop}.
+    Number of generations: {n_generations}.
+    Mutation probability: {p_mutation:.2f}.
+    Vertical probability: {p_vertical:.2f}.
+    Horizontal probability: {p_horizontal:.2f}.
+    -------------------------------------------------------------------
+    """
+    if print_terminal:
+        print(header)
+    else:
+        de_u.log(header, 'w')
+
+    
+    # Initialize shared_memory in multiprocessing
+    shm_pop = shared_memory.SharedMemory(create=True, size=population.nbytes)
+    shm_fit = shared_memory.SharedMemory(create=True, size=fitness.nbytes)
+
+    pop = np.ndarray(population.shape, dtype=np.int64, buffer=shm_pop.buf)
+    fit = np.ndarray(fitness.shape, dtype=np.float64, buffer=shm_fit.buf)
+
+    pop[:] = population[:]
+    fit[:] = fitness[:]
+
+    pop_name = shm_pop.name
+    fit_name = shm_fit.name
+
+    pop_shape = pop.shape
+    fit_shape = fit.shape
+
+    i_idx = Value('i', i_start)
+    i = i_start
+    # Use Pool for parallel computing
+    pool = Pool(processes=n_processes, initializer=pool_initializer)
+    pop_idx = range(n_pop)
+
+    try:
+        # Initial evaluation
+        pool.map(evaluate,pop_idx)
+        #for j in range(n_pop):
+        #    if fit[0,j] == -np.inf:
+        #        fit[0,j] = evaluate(pop[j, :, :])
+
+        for i in range(i_start,n_generations-1):
+
+            # Select with elitism
+            pop_new = np.zeros(pop.shape, int)
+            #fit_new = np.full(fit.shape, -np.inf)
+            pop_new[0] = pop[np.argmax(fit[i,:])]
+            fit[i+1,0] = fit[i,np.argmax(fit[i,:])]
+            for j in range(1, n_pop):
+                pop_new[j], fit[i+1,j] = tournament(
+                    pop, fit[i,:])
+            pop[:] = pop_new[:]
+            #fit = fit_new
+
+            # Crossover
+            for j in range(1, n_pop-1, 2):
+                if np.random.rand() < p_vertical:
+                    pop[j], pop[j+1] = vertical_crossover(pop[j], pop[j+1])
+                    fit[i,j], fit[i,j+1] = -np.inf, -np.inf
+                if np.random.rand() < p_horizontal:
+                    pop[j], pop[j+1] = horizontal_crossover(pop[j], pop[j+1])
+                    fit[i+1,j], fit[i+1,j+1] = -np.inf, -np.inf
+
+            # Mutation
+            for j in range(1, n_pop):
+                if np.random.rand():
+                    pop[j] = mutation(pop[j])
+                    fit[i+1,j] = -np.inf
+
+            # Evaluate new individuals
+            i_idx.value += 1
+            pool.map(evaluate, pop_idx)
+            #for j in range(n_pop):
+            #    if fit[i+1,j] == -np.inf:
+            #        fit[i+1,j] = evaluate(pop[j, :, :])
+
+            
+            status = f"{i} generations completed. RBER: best: {np.max(fit[i+1,:]):.2f}, min: {np.min(fit[i+1,:]):.2f}, mean: {np.mean(fit[i+1,:]):.2f}, variance: {np.var(fit[i+1,:]):.2f}.                                "
+            if print_terminal:    
+                print(status, end='\r', flush=True)
+            else:
+                de_u.log(status, 'a')
+
+            if i % int(cfg_de.get("save_interval")) == 0:
+                de_u.save_population(pop,fit,i)
+
+
+    
+        status = f"""
+    -------------------------------------------------------------------
+    Finished!
+    ===================================================================
+            """
+        if print_terminal:    
+            print(status)
+        else:
+            de_u.log(status,'a')
+
+    finally:
+        de_u.save_population(pop, fit, i)
+        shm_pop.close()
+        shm_fit.close()
+        shm_pop.unlink()
+        shm_fit.unlink()
