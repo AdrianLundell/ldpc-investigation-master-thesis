832c8a619af7817b5ee42cad1dcf5587a9263832
Adrian Lundell <adrian.lundell@gmail.com>
2022-07-11 19:10:04 +0200

    Add BSC

--- python-files/compute_dmc/compute_shannon.py
+++ python-files/compute_dmc/compute_shannon.py
@@ -1,63 +1,89 @@
+"""
+This scripts finds the maximum decodeable noise level given a code rate for three different channels:
+1. BSC
+2. Discretized AWGN [only for small code rates]
+3. AWGN
+"""
+#%% Imports
 import dmc_utils
-import yaml
 import numpy as np 
 import scipy.stats as stats 
 
-skew = 0.5
+# %% Binary symmetric channel
 code_rate = 1 - 9/73
-is_symmetric = (skew == 0.5)
-
-def compute_capacity(rber, skew):
-    sigma = dmc_utils.rber_to_sigma(rber, skew)
-    sigma1 = (1-skew)*sigma
-    sigma2 = skew*sigma 
-
-    t, capacity = dmc_utils.optimize_thresholds(sigma1, sigma2, symmetric = is_symmetric)
-    return capacity
 
 min = 0.001
 max = 0.5
 tol = 1e-7
 
 while max - min > tol:
-    x = (min + max)/2
-    capacity = compute_capacity(x, skew) 
-    result = (1-capacity) - code_rate
+    rber = (min + max)/2
 
-    if result < 0:
-        min = x
-    elif result > 0:
-        max = x
+    capacity = 1 - (rber * np.log2(1/rber) + (1-rber)*np.log2(1/(1-rber)))
+    result = capacity - code_rate
+
+    if result > 0:
+        min = rber
+    elif result < 0:
+        max = rber
     else:
         raise Exception
 
 rber = (min + max)/2
-sigma = dmc_utils.rber_to_sigma(rber, skew)
-print(f"The shannon limit for the discretized A-AWGN channel ({code_rate}, {skew}) is {rber}(RBER)/{sigma}(SIGMA)")
+sigma_tot = dmc_utils.rber_to_sigma(rber, 0.5)
+print(f"The shannon limit for the BSC with code rate ({code_rate:.4}) is {rber:.4}(RBER)/{sigma_tot:.4}(SIGMA)")
 
-
-min = 0.001
-max = 0.5
+#%% Discretized AWGN
+###########################################
+####!DOES NOT MANGAGE CODE RATES >0.74!####
+###########################################
+code_rate = 1 - 9/73
+min = 0.1
+max = 5
 tol = 1e-7
 
-#Continous symmetric case
 while max - min > tol:
-    x = (min + max)/2
+    sigma = (min + max)/2
+    t, capacity = dmc_utils.optimize_thresholds(sigma, sigma, symmetric=True)
+    result = capacity - code_rate
+        
+    if result > 0:
+        min = sigma
+    elif result < 0:
+        max = sigma
+    else:
+        pass
 
-    s = dmc_utils.rber_to_sigma(x, skew)
-    range = np.linspace(-10, 10, 1000)
-    cdf = 1/np.sqrt(8 * np.pi * s**2) * (np.exp(-(range-1)**2/(2*s**2)) + np.exp(-(range+1)**2/(2*s**2)))
-    entropy = cdf * np.log2(cdf)
-    capacity = -np.trapz(entropy, range) - 1/2*np.log2(2*np.pi*np.e*s**2)
-    result = (1-capacity) - code_rate
+sigma = (min + max)/2
+rber = stats.norm.cdf(0, 1, sigma)
+sigma_tot = sigma*2
+print(f"""The shannon limit for the discretized A-AWGN channel ({code_rate:.4}, {0.5}) is {rber:.4}(RBER)/{sigma_tot:.4}(SIGMA)""")
+#%% AWGN 
+code_rate = 1 - 9/73
+min = 0.1
+max = 5
+tol = 1e-7
 
-    if result < 0:
-        min = x
-    elif result > 0:
-        max = x
+while max - min > tol:
+    sigma = (min + max)/2
+
+    range = np.arange(-20,20,0.0001)
+    cdf = 1/np.sqrt(8 * np.pi * sigma**2) * (np.exp(-(range-1)**2/(2*sigma**2)) + np.exp(-(range+1)**2/(2*sigma**2)))
+    entropy = cdf * np.log2(np.sqrt(2*np.pi*np.e*sigma**2)*cdf)
+    entropy[np.isnan(entropy)] = 0
+    capacity = -np.trapz(entropy, range) 
+    result = capacity - code_rate
+
+    if result > 0:
+        min = sigma
+    elif result < 0:
+        max = sigma
     else:
         raise Exception
 
-rber = (min + max)/2
-sigma = dmc_utils.rber_to_sigma(rber, skew)
-print(f"The shannon limit for the continous AWGN channel ({code_rate}) is {rber}(RBER)/{sigma}(SIGMA)")
+sigma = (min + max)/2
+rber = stats.norm.cdf(0, 1, sigma)
+sigma_tot = sigma*2
+print(f"The shannon limit for the continous AWGN channel ({code_rate:.4}) is {rber:.4}(RBER)/{sigma_tot:.4}(SIGMA)")
+
+# %%
