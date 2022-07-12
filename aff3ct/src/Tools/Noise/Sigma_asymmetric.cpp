a91edb1ff10ceaf05d73b3ffecedf3af2901a312
Fredrik Blomgren <frebl@student.chalmers.se>
2022-07-12 14:39:26 +0200

    Make sigma asymmetric to SLC

--- aff3ct/src/Tools/Noise/Sigma_asymmetric.cpp
+++ aff3ct/src/Tools/Noise/Sigma_asymmetric.cpp
@@ -9,58 +9,42 @@ template <typename R>
 Sigma_asymmetric<R>::Sigma_asymmetric()
 	: Sigma<R>()
 {
-	this->sigma_generator = tools::Random_sigma_generator<R>();
+	this->sigmas = std::vector<R>(2, (R)0.0);
 }
 
 template <typename R>
 Sigma_asymmetric<R>::Sigma_asymmetric(const Sigma_asymmetric<R> &other)
 	: Sigma_asymmetric<R>()
 {
-	this->min_sigma = other.min_sigma;
 	this->sigmas = other.sigmas;
-	this->sigma_ratios = other.sigma_ratios;
+	this->skew = other.skew;
 	this->sigmas_exist = other.sigmas_exist;
 	this->set_noise(other.get_noise(), other.get_ebn0(), other.get_esn0());
 }
 
 template <typename R>
-void Sigma_asymmetric<R>::set_sigmas(R sigma_tot, unsigned n_sigmas, R m_s, R ebn0, R esn0)
+void Sigma_asymmetric<R>::set_sigmas(R sigma_ave, R ebn0, R esn0, R skew)
 {
-	this->min_sigma = m_s;
-	this->sigmas = std::vector<R>(n_sigmas, (R)0.0);
-	this->sigma_ratios = std::vector<R>(n_sigmas - 1, (R)0.0);
-	this->set_noise(sigma_tot, ebn0, esn0);
+	this->set_noise(sigma_ave, ebn0, esn0);
+	this->skew = skew;
+
+	this->sigmas[0] = (2 * sigma_ave) * (1 - skew);
+	this->sigmas[1] = (2 * sigma_ave) * skew;
+
 	this->sigmas_exist = true;
 	this->check();
-}
-
-template <typename R>
-void Sigma_asymmetric<R>::set_sigmas(std::vector<R>& sigmas)
-{
-	R min = (R) sigmas[0];
-	R tot = (R) 0;
-	R ebn0, esn0;
-
-	for (auto sigma : sigmas)
-	{
-		tot += sigma;
-		if (sigma < min) min = sigma;
-	}
-
-	this->min_sigma = min;
-	this->sigmas = sigmas;
-	this->sigma_ratios = std::vector<R>(sigmas.size() - 1, (R)0.0);
-	this->set_noise(tot, ebn0, esn0);
-	this->sigmas_exist = true;
-	this->check();
-
-	this->compute_sigma_ratios();
 }
 
 template <typename R>
 R Sigma_asymmetric<R>::get_sigma(unsigned voltage_index) const
 {
 	return sigmas[voltage_index];
+}
+
+template <typename R>
+R Sigma_asymmetric<R>::get_sigma_ave() const
+{
+	return this->get_noise();
 }
 
 template <typename R>
@@ -76,74 +60,20 @@ bool Sigma_asymmetric<R>::has_sigmas() const noexcept
 }
 
 template <typename R>
-void Sigma_asymmetric<R>::generate_sigmas()
-{
-	if (this->sigmas_exist)
-	{
-		this->sigma_generator.generate(sigmas, this->get_noise(), min_sigma);
-		this->compute_sigma_ratios();
-	}
-	else
-	{
-		std::stringstream message;
-		message << "The SIGMA_ASYMMETRIC parameters have not been set. Call set_sigmas() before calling this method.";
-		throw tools::logic_error(__FILE__, __LINE__, __func__, message.str());
-	}
-}
-
-template <typename R>
-void Sigma_asymmetric<R>::compute_sigma_ratios()
-{
-	for (unsigned i = 0; i < sigma_ratios.size(); i++)
-	{
-		R sigma_i = sigmas[i];
-		R sigma_ii = sigmas[i + 1];
-		this->sigma_ratios[i] = sigma_i * sigma_i / (sigma_i * sigma_i + sigma_ii * sigma_ii);
-	}
-}
-
-template <typename R>
-void Sigma_asymmetric<R>::set_seed(const int seed)
-{
-	this->sigma_generator.set_seed(seed);
-}
-
-template <typename R>
 R Sigma_asymmetric<R>::get_threshold_noise(unsigned threshold_index) const
 {
 	return (R)-10 * log10(pow(sigmas[threshold_index], (R)2.0) + pow(sigmas[threshold_index + 1], (R)2.0));
 }
 
 template <typename R>
-R Sigma_asymmetric<R>::get_ratio(unsigned threshold_index) const
-{
-	return this->sigma_ratios[threshold_index];
-}
-
-template <typename R>
 void Sigma_asymmetric<R>::check() const
 {
 	R n = this->get_noise();
-	if (n <= (R)0)
-	{
-		std::stringstream message;
-		message << "The SIGMA_ASYMMETRIC total sigma '_n' has to be greater than 0 ('_n' = " << n << ").";
-		throw tools::invalid_argument(__FILE__, __LINE__, __func__, message.str());
-	}
 
-	if (min_sigma <= (R)0)
+	if (skew <= (R)0 || skew >= (R)1)
 	{
 		std::stringstream message;
-		message << "The SIGMA_ASYMMETRIC minimum sigma 'min_sigma' has to be greater than 0 ('min_sigma' = " << min_sigma << ").";
-		throw tools::invalid_argument(__FILE__, __LINE__, __func__, message.str());
-	}
-
-	unsigned n_sigmas = sigmas.size();
-	R min_sigma_tot = min_sigma * n_sigmas;
-	if (n < min_sigma_tot)
-	{
-		std::stringstream message;
-		message << "The SIGMA_ASYMMETRIC total sigma '_n' has to be greater than " << n_sigmas << "*min_sigma  ('n' = " << n << ", 'min_sigma' =" << min_sigma << ").";
+		message << "The SIGMA_ASYMMETRIC skewness 'skew' has to be greater than 0 and smaller than 1" << skew << ").";
 		throw tools::invalid_argument(__FILE__, __LINE__, __func__, message.str());
 	}
 }
