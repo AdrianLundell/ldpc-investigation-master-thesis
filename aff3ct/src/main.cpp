b235b39150288f10ded926fda4010889ae67984c
Fredrik Blomgren <frebl@student.chalmers.se>
2022-07-12 14:42:42 +0200

    Add skewness input variable and adjust main file to previous changes on classes

--- aff3ct/src/main.cpp
+++ aff3ct/src/main.cpp
@@ -7,28 +7,28 @@
 #include <aff3ct.hpp>
 #include <aff3ct_extension.hpp>
 
-
 using namespace aff3ct;
 
-struct params 
+struct params
 {
-	int K = 18688 - 2304;	 // number of information bits
-	int N = 18688;	 		 // codeword size
-	int fe = 100;			 // number of frame errors
-	int seed = 0;			 // PRNG seed for the AWGN channel
-	float ebn0_min = 0.f;	 // minimum SNR value
-	float ebn0_max = 10.f;   // maximum SNR value
-	float ebn0_step = .5f;   // SNR step
-	float R;				 // code rate (R=K/N)
-	float min_sigma = 0.5;   // Set minimum value for individual sigmas (NOT IN USE)
-	
-    std::vector<float> voltage_levels{-1, 1};
-    std::string reader_fpath = "data/reader_files/Symmetric_single_soft.csv";
-	std::string output_fpath = "../../python-files/PyBER/data/Symmetric_soft_test.txt";
-	std::string ldpc_fpath = "../../python-files/Codec/1_edge_regular.qc";
-    int page_type = tools::Flash_reader<float, float>::lower;
-    int read_type = tools::Flash_reader<float, float>::soft_single;
-    int cell_type = tools::Flash_cell::SLC;
+	int K = 18688 - 2304;	// number of information bits
+	int N = 18688;			// codeword size
+	int fe = 100;			// number of frame errors
+	int seed = 0;			// PRNG seed for the AWGN channel
+	float ebn0_min = -0.7f; // minimum SNR value
+	float ebn0_max = 7.3f;	// maximum SNR value
+	float ebn0_step = .5f;	// SNR step
+	float R;				// code rate (R=K/N)
+	float min_sigma = 0.5;	// Set minimum value for individual sigmas (NOT IN USE)
+	float skew = 0.5;
+
+	std::vector<float> voltage_levels{-1, 1};
+	std::string reader_fpath = "../../python-files/compute_dmc/data/4_05_AWGN_aff3ct.csv";
+	std::string output_fpath = "../../python-files/analysis/aff3ct_simulations/results/data/discrete_run1_v2.txt";
+	std::string ldpc_fpath = "../../python-files/mm_qc_pega/data/discrete_run1_v2.qc";
+	int page_type = tools::Flash_reader<float, float>::lower;
+	int read_type = tools::Flash_reader<float, float>::soft_single;
+	int cell_type = tools::Flash_cell::SLC;
 
 	std::vector<uint32_t> info_bits = std::vector<uint32_t>(K);
 };
@@ -58,7 +58,7 @@ void init_buffers(const params &p, buffers &b);
 
 struct utils
 {
-	std::unique_ptr<tools::Sigma_asymmetric<>> noise;	     // a sigma noise type
+	std::unique_ptr<tools::Sigma_asymmetric<>> noise;		 // a sigma noise type
 	std::vector<std::unique_ptr<tools::Reporter>> reporters; // list of reporters dispayed in the terminal
 	std::unique_ptr<tools::Terminal_std> terminal;			 // manage the output text in the terminal
 	std::ofstream output;
@@ -79,7 +79,7 @@ int main(int argc, char **argv)
 	init_modules(p, m); // create and initialize the modules
 	buffers b;
 	init_buffers(p, b); // create and initialize the buffers required by the modules
-	utils u;	
+	utils u;
 	init_utils(p, m, u); // create and initialize the utils
 
 	print_params(u.output);
@@ -92,10 +92,10 @@ int main(int argc, char **argv)
 	{
 		// compute the current sigma for the channel noise
 		const auto esn0 = tools::ebn0_to_esn0(ebn0, p.R);
-		const auto tot_sigma = tools::esn0_to_sigma(esn0);
+		const auto sigma_ave = tools::esn0_to_sigma(esn0);
 
 		// update the sigma of the modem and the channel
-		u.noise->set_sigmas(tot_sigma, p.voltage_levels.size(), tot_sigma/2.f ,ebn0, esn0);
+		u.noise->set_sigmas(sigma_ave, ebn0, esn0, p.skew);
 		m.channel->set_noise(*u.noise);
 
 		// display the performance (BER and FER) in real time (in a separate thread)
@@ -105,12 +105,11 @@ int main(int argc, char **argv)
 		while (!m.monitor->fe_limit_achieved() && !u.terminal->is_interrupt())
 		{
 			m.source->generate(b.ref_bits);
-            m.encoder->encode(b.ref_bits, b.enc_bits);
-            m.modem->modulate(b.enc_bits, b.voltage_level_indexes);          
-     
-	        m.channel->generate_sigmas();
-	        m.modem->set_noise(*m.channel);
-			
+			m.encoder->encode(b.ref_bits, b.enc_bits);
+			m.modem->modulate(b.enc_bits, b.voltage_level_indexes);
+
+			m.modem->set_noise(*m.channel);
+
 			m.channel->add_noise(b.voltage_level_indexes.data(), b.noisy_voltage_levels.data());
 			m.modem->demodulate(b.noisy_voltage_levels, b.noisy_bits);
 			m.decoder->decode_siho(b.noisy_bits, b.dec_bits);
@@ -155,29 +154,28 @@ void init_params(params &p)
 	std::cout << "#" << std::endl;
 }
 
-
 void init_modules(const params &p, modules &m)
 {
 	m.source = std::unique_ptr<module::Source_random<>>(new module::Source_random<>(p.K));
-	
+
 	const tools::Sparse_matrix H = tools::LDPC_matrix_handler::read(p.ldpc_fpath);
 	m.encoder = std::unique_ptr<module::Encoder_LDPC_from_QC<>>(new module::Encoder_LDPC_from_QC<>(p.K, p.N, H));
- 
-    tools::Flash_cell cell(p.cell_type);
-    tools::Flash_reader<float, float> reader(p.page_type, p.read_type, p.reader_fpath);
+
+	tools::Flash_cell cell(p.cell_type);
+	tools::Flash_reader<float, float> reader(p.page_type, p.read_type, p.reader_fpath);
 	m.modem = std::unique_ptr<module::Modem_flash_page<>>(new module::Modem_flash_page<>(p.N, cell, reader));
 
-    tools::Sigma_asymmetric<float> sigmas;
-    sigmas.set_sigmas(1, 2, 0.1 , 0, 0);
+	tools::Sigma_asymmetric<float> sigmas;
+	sigmas.set_sigmas(1, 2, 2, 0.5);
 
 	m.channel = std::unique_ptr<module::Channel_AWGN_asymmetric<>>(new module::Channel_AWGN_asymmetric<float>(p.N, p.voltage_levels, sigmas));
-	
-    m.decoder = std::unique_ptr<module::Decoder_LDPC_BP_flooding_SPA<>>(new module::Decoder_LDPC_BP_flooding_SPA<>(
-	p.K, 
-	p.N, 
-	10, 
-	H,
-	p.info_bits));
+
+	m.decoder = std::unique_ptr<module::Decoder_LDPC_BP_flooding_SPA<>>(new module::Decoder_LDPC_BP_flooding_SPA<>(
+		p.K,
+		p.N,
+		10,
+		H,
+		p.info_bits));
 	m.monitor = std::unique_ptr<module::Monitor_BFER<>>(new module::Monitor_BFER<>(p.K, p.fe));
 };
 
@@ -205,77 +203,76 @@ void init_utils(const params &p, const modules &m, utils &u)
 	u.terminal = std::unique_ptr<tools::Terminal_std>(new tools::Terminal_std(u.reporters));
 
 	u.output.open(p.output_fpath, std::ios_base::app);
-
 }
 
 void print_params(std::ostream &stream)
 {
-//OBS Hardcoded and false parameters
-stream  << "[metadata]" << std::endl;
-stream << "ci=on" << std::endl;
-stream << "command=None" << std::endl;
-stream << "title=BCH (7,4,1)" << std::endl;
-stream << "" << std::endl;
-stream << "[trace]" << std::endl;
-stream << "# ----------------------------------------------------" << std::endl;
-stream << "# ---- A FAST FORWARD ERROR CORRECTION TOOLBOX >> ----" << std::endl;
-stream << "# ----------------------------------------------------" << std::endl;
-stream << "# Parameters :" << std::endl;
-stream << "# * Simulation ------------------------------------" << std::endl;
-stream << "#    ** Type                   = BFER" << std::endl;
-stream << "#    ** Type of bits           = int32" << std::endl;
-stream << "#    ** Type of reals          = float32" << std::endl;
-stream << "#    ** Date (UTC)             = 2022-05-10" << std::endl;
-stream << "#    ** Git version            = " << std::endl;
-stream << "#    ** Code type (C)          = QC-LDPC" << std::endl;
-stream << "#    ** Noise range            = 0 -> 10 dB" << std::endl;
-stream << "#    ** Noise type (E)         = EBN0" << std::endl;
-stream << "#    ** Seed                   = 0" << std::endl;
-stream << "#    ** Statistics             = off" << std::endl;
-stream << "#    ** Debug mode             = off" << std::endl;
-stream << "#    ** Multi-threading (t)    = no" << std::endl;
-stream << "#    ** Coset approach (c)     = no" << std::endl;
-stream << "#    ** Coded monitoring       = no" << std::endl;
-stream << "#    ** Bad frames tracking    = off" << std::endl;
-stream << "#    ** Bad frames replay      = off" << std::endl;
-stream << "#    ** Bit rate               = -" << std::endl;
-stream << "#    ** Inter frame level      = -" << std::endl;
-stream << "# * Source ----------------------------------------" << std::endl;
-stream << "#    ** Type                   = RAND" << std::endl;
-stream << "#    ** Implementation         = STD" << std::endl;
-stream << "#    ** Info. bits (K_info)    = 16 384" << std::endl;
-stream << "# * Codec -----------------------------------------" << std::endl;
-stream << "#    ** Type                   = LDPC" << std::endl;
-stream << "#    ** Info. bits (K)         = 16 384" << std::endl;
-stream << "#    ** Codeword size (N_cw)   = 18 688" << std::endl;
-stream << "#    ** Frame size (N)         = 18 688" << std::endl;
-stream << "#    ** Code rate              = 0.877" << std::endl;
-stream << "# * Encoder ---------------------------------------" << std::endl;
-stream << "#    ** Type                   = LDPC" << std::endl;
-stream << "#    ** Systematic             = No" << std::endl;
-stream << "# * Decoder ---------------------------------------" << std::endl;
-stream << "#    ** Type (D)               = BP" << std::endl;
-stream << "#    ** Implementation         = STD" << std::endl;
-stream << "#    ** Systematic             = No" << std::endl;
-stream << "#    ** Galois field order (m) = 2" << std::endl;
-stream << "#    ** Correction power (T)   = 1" << std::endl;
-stream << "# * Modem -----------------------------------------" << std::endl;
-stream << "#    ** Type                   = Flash" << std::endl;
-stream << "#    ** Implementation         = STD" << std::endl;
-stream << "#    ** Bits per symbol        = 1" << std::endl;
-stream << "#    ** Sampling factor        = 1" << std::endl;
-stream << "#    ** Sigma square           = on" << std::endl;
-stream << "# * Channel ---------------------------------------" << std::endl;
-stream << "#    ** Type                   = BiAWGN" << std::endl;
-stream << "#    ** Implementation         = STD" << std::endl;
-stream << "#    ** Complex                = off" << std::endl;
-stream << "#    ** Add users              = off" << std::endl;
-stream << "# * Monitor ---------------------------------------" << std::endl;
-stream << "#    ** Frame error count (e)  = 100" << std::endl;
-stream << "#    ** Compute mutual info    = no" << std::endl;
-stream << "# * Terminal --------------------------------------" << std::endl;
-stream << "#    ** Enabled                = yes" << std::endl;
-stream << "#    ** Frequency (ms)         = 500" << std::endl;
-stream << "#" << std::endl;
-stream << "# The simulation is running..." << std::endl;
+	// OBS Hardcoded and false parameters
+	stream << "[metadata]" << std::endl;
+	stream << "ci=on" << std::endl;
+	stream << "command=None" << std::endl;
+	stream << "title=BCH (7,4,1)" << std::endl;
+	stream << "" << std::endl;
+	stream << "[trace]" << std::endl;
+	stream << "# ----------------------------------------------------" << std::endl;
+	stream << "# ---- A FAST FORWARD ERROR CORRECTION TOOLBOX >> ----" << std::endl;
+	stream << "# ----------------------------------------------------" << std::endl;
+	stream << "# Parameters :" << std::endl;
+	stream << "# * Simulation ------------------------------------" << std::endl;
+	stream << "#    ** Type                   = BFER" << std::endl;
+	stream << "#    ** Type of bits           = int32" << std::endl;
+	stream << "#    ** Type of reals          = float32" << std::endl;
+	stream << "#    ** Date (UTC)             = 2022-05-10" << std::endl;
+	stream << "#    ** Git version            = " << std::endl;
+	stream << "#    ** Code type (C)          = QC-LDPC" << std::endl;
+	stream << "#    ** Noise range            = 0 -> 10 dB" << std::endl;
+	stream << "#    ** Noise type (E)         = EBN0" << std::endl;
+	stream << "#    ** Seed                   = 0" << std::endl;
+	stream << "#    ** Statistics             = off" << std::endl;
+	stream << "#    ** Debug mode             = off" << std::endl;
+	stream << "#    ** Multi-threading (t)    = no" << std::endl;
+	stream << "#    ** Coset approach (c)     = no" << std::endl;
+	stream << "#    ** Coded monitoring       = no" << std::endl;
+	stream << "#    ** Bad frames tracking    = off" << std::endl;
+	stream << "#    ** Bad frames replay      = off" << std::endl;
+	stream << "#    ** Bit rate               = -" << std::endl;
+	stream << "#    ** Inter frame level      = -" << std::endl;
+	stream << "# * Source ----------------------------------------" << std::endl;
+	stream << "#    ** Type                   = RAND" << std::endl;
+	stream << "#    ** Implementation         = STD" << std::endl;
+	stream << "#    ** Info. bits (K_info)    = 16 384" << std::endl;
+	stream << "# * Codec -----------------------------------------" << std::endl;
+	stream << "#    ** Type                   = LDPC" << std::endl;
+	stream << "#    ** Info. bits (K)         = 16 384" << std::endl;
+	stream << "#    ** Codeword size (N_cw)   = 18 688" << std::endl;
+	stream << "#    ** Frame size (N)         = 18 688" << std::endl;
+	stream << "#    ** Code rate              = 0.877" << std::endl;
+	stream << "# * Encoder ---------------------------------------" << std::endl;
+	stream << "#    ** Type                   = LDPC" << std::endl;
+	stream << "#    ** Systematic             = No" << std::endl;
+	stream << "# * Decoder ---------------------------------------" << std::endl;
+	stream << "#    ** Type (D)               = BP" << std::endl;
+	stream << "#    ** Implementation         = STD" << std::endl;
+	stream << "#    ** Systematic             = No" << std::endl;
+	stream << "#    ** Galois field order (m) = 2" << std::endl;
+	stream << "#    ** Correction power (T)   = 1" << std::endl;
+	stream << "# * Modem -----------------------------------------" << std::endl;
+	stream << "#    ** Type                   = Flash" << std::endl;
+	stream << "#    ** Implementation         = STD" << std::endl;
+	stream << "#    ** Bits per symbol        = 1" << std::endl;
+	stream << "#    ** Sampling factor        = 1" << std::endl;
+	stream << "#    ** Sigma square           = on" << std::endl;
+	stream << "# * Channel ---------------------------------------" << std::endl;
+	stream << "#    ** Type                   = BiAWGN" << std::endl;
+	stream << "#    ** Implementation         = STD" << std::endl;
+	stream << "#    ** Complex                = off" << std::endl;
+	stream << "#    ** Add users              = off" << std::endl;
+	stream << "# * Monitor ---------------------------------------" << std::endl;
+	stream << "#    ** Frame error count (e)  = 100" << std::endl;
+	stream << "#    ** Compute mutual info    = no" << std::endl;
+	stream << "# * Terminal --------------------------------------" << std::endl;
+	stream << "#    ** Enabled                = yes" << std::endl;
+	stream << "#    ** Frequency (ms)         = 500" << std::endl;
+	stream << "#" << std::endl;
+	stream << "# The simulation is running..." << std::endl;
 }
